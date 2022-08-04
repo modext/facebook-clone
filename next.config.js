cb1f7fa7de9e0bef8608d353d508eb4fe1e392d8
Your Name <33578391+modext@users.noreply.github.com>
2022-08-04 08:34:26 +0100

    header done

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ]
+  }
+} 
