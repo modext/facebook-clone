6ce5c780f4e1891a8015229045fd476a1df2554a
Your Name <33578391+modext@users.noreply.github.com>
2022-08-05 09:07:09 +0100

    feeds done

--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import Stories from './Stories'
 
 function Feed() {
   return (
-    <div>
-        Fe
+    <div className='flex-grow h-screen pb-44 pt-6 mr-4 
+    xl:mr-40 overflow-y-auto'>
+        <div className='mx-auto max-w-md md:max-w-lg 
+        lg:max-w-2xl'>
+            {/* stories */}
+            <Stories />
+            {/* Input box */}
+            {/* posts */}
+        </div>        
     </div>
   )
 }
