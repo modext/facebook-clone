fca2c4ded2e9b4da59f1a6fcba4b86affba8b88a
Your Name <33578391+modext@users.noreply.github.com>
2022-08-11 21:26:51 +0100

    deleted

--- components/Posts.js
+++ components/Posts.js
@@ -37,17 +37,7 @@ function Posts() {
             postImage= {post.postImage}
         />
       ))}
-        {/* {realtimePosts.docs.map(post =>{
-            <Post 
-                key={post.id}
-                name={post.data().name}
-                message={post.data().message}
-                email= {post.data().email}
-                image= {post.data().image}
-                postImage= {post.data().postImage}
-            
-            />
-        })} */}
+        
         {doc.id}
     </div>
   )
