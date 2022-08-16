63dc94eb763383ac81f4e4f35b45af2071fe671f
Your Name <33578391+modext@users.noreply.github.com>
2022-08-16 23:15:04 +0100

    Add Widget

---
+++ components/Widgets.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const contacts = [
+    {src: "https://links.papareact.com/f0p", name: "Jeff Bezoz"},
+    {src: "https://links.papareact.com/kxk", name: "Elon Musk"},
+    {src: "https://links.papareact.com/zvy", name: "Bill Gates"},
+    {src: "https://links.papareact.com/snf", name: "Mark ZUckerberg"},
+    {src: "https://links.papareact.com/d0c", name: "Harry Porter"},
+    {src: "https://links.papareact.com/6gg", name: "The Queen"},
+    {src: "https://links.papareact.com/r57", name: "James Bond"},
+]
+
+function Widgets() {
+  return (
+    <div>
+        hand
+    </div>
+  )
+}
+
+export default Widgets
